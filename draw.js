0ff8a22188862b8bb645e12134fc4e676d124223
Cassidy Williams <cassidoo@outlook.com>
2014-03-12 01:48:27 -0500

    Setup files

---
+++ draw.js
@@ -0,0 +1,2 @@
+
+// going to use https://github.com/antimatter15/ocrad.js for OCR stuff
---
+++ pagecontrol.js
@@ -0,0 +1,15 @@
+function inputMethod(iMethod)
+{
+	if(iMethod === "type")
+	{
+		//display type stuff
+	}
+	else if(iMethod === "draw")
+	{
+		//display draw stuff
+	}
+	else
+	{
+		alert("what the heck is happening");
+	}
+}
