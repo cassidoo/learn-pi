f9e858b7fb13e9e2e08bde4da5c1956be2aaf584
Cassidy Williams <cassidoo@outlook.com>
2014-03-16 22:01:51 -0500

    Adding some more look/feel/functionality

--- pagecontrol.js
+++ pagecontrol.js
@@ -13,3 +13,19 @@ function inputMethod(iMethod)
 		alert("what the heck is happening");
 	}
 }
+
+function key_detect_calc(event)
+{
+    if((event.keyCode > 47) && (event.keyCode < 58))
+    {
+        var num = event.keyCode - 48;
+        numinput(num);
+    }
+}
+
+function numinput(num)
+{
+    
+}
+
+// going to use https://github.com/antimatter15/ocrad.js for OCR stuff
