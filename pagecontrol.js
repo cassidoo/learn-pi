cc95facc9cd1da45b4fee2b35100f4c747a4331b
Cassidy Williams <cassidoo@outlook.com>
2014-03-18 20:34:18 -0500

    Finished typing stuff

--- pagecontrol.js
+++ pagecontrol.js
@@ -16,22 +16,30 @@ function inputMethod(iMethod)
 	}
 }
 
-function key_detect_calc(event)
+function reset()
+{
+    cursor = 0;
+    document.getElementById("resultat").value = "";
+}
+
+document.onkeydown = function(event)
 {
     if((event.keyCode > 47) && (event.keyCode < 58))
     {
         var num = event.keyCode - 48;
-        numinput(num);
+        numinput(num.toString());
     }
-}
-
-function reset()
-{
-    cursor = 0;
-}
+    else
+        console.log("Someone didn't type a number. Idiot.");
+};
 
 function numinput(num)
 {
+    document.getElementById("resultat").value = document.getElementById("resultat").value + num;
+    if(document.getElementById("resultat").value.length > 7)
+    {
+        document.getElementById("resultat").value = document.getElementById("resultat").value.substring(1);   
+    }
     if(num < 0)
     {
         alert("what the heck is happening");
@@ -43,7 +51,7 @@ function numinput(num)
     }
     else
     {
-        alert("Dang. You should have typed " + pi.substring(cursor, cursor+1) + ". Start over!");
+        alert("Dang. The next part is actually " + pi.substring(cursor, cursor+9) + ". Start over!");
         reset();
     }
 }
