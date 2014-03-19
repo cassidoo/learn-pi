a735a14abd41b44eefbd795f246c9e078fadb793
Cassidy Williams <cassidoo@outlook.com>
2014-03-18 21:51:55 -0500

    Condensing and making things pretty

--- pagecontrol.js
+++ pagecontrol.js
@@ -2,56 +2,57 @@ var cursor = 0;
 
 function inputMethod(iMethod)
 {
-	if(iMethod === "type")
-	{
-		//display type stuff
-	}
-	else if(iMethod === "draw")
-	{
-		//display draw stuff
-	}
-	else
-	{
-		alert("what the heck is happening");
-	}
+    document.getElementById("opening").style.display = "none";
+    if(iMethod === "type")
+    {
+        document.getElementById("typing").style.display = "block";
+
+        document.onkeydown = function(event)
+        {
+            if((event.keyCode > 47) && (event.keyCode < 58))
+            {
+                var num = event.keyCode - 48;
+                numinput(num.toString());
+            }
+            else
+                console.log("Someone didn't type a number. Idiot.");
+        };
+    }
+    else if(iMethod === "draw")
+    {
+        document.getElementById("drawing").style.display = "block";
+    }
+    else
+    {
+        alert("what the heck is happening");
+    }
 }
 
 function reset()
 {
     cursor = 0;
-    document.getElementById("resultat").value = "";
+    document.getElementById("resultat").value = "3.";
 }
-
-document.onkeydown = function(event)
-{
-    if((event.keyCode > 47) && (event.keyCode < 58))
-    {
-        var num = event.keyCode - 48;
-        numinput(num.toString());
-    }
-    else
-        console.log("Someone didn't type a number. Idiot.");
-};
 
 function numinput(num)
 {
     document.getElementById("resultat").value = document.getElementById("resultat").value + num;
     if(document.getElementById("resultat").value.length > 7)
     {
-        document.getElementById("resultat").value = document.getElementById("resultat").value.substring(1);   
+        document.getElementById("resultat").value = document.getElementById("resultat").value.substring(1);
     }
     if(num < 0)
     {
         alert("what the heck is happening");
     }
-    
-    if(num === pi.substring(cursor, cursor+1))
+
+    if(num === pi.substring(cursor, cursor + 1))
     {
         cursor++;
     }
     else
     {
-        alert("Dang. The next part is actually " + pi.substring(cursor, cursor+9) + ". Start over!");
+        alert("Dang. The next part is actually " + pi.substring(cursor, cursor + 9) + ". Start over!");
         reset();
     }
 }
